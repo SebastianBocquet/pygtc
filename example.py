39a4a99b3a9bb4d4cd5b4ede23b7948bca90d2cc
Sebastian Bocquet <sbocquet@anl.gov>
2016-07-15 15:40:25 -0500

    lots of work actually...

--- example.py
+++ example.py
@@ -1,7 +1,27 @@
+from matplotlib import pyplot as plt
 import numpy as np
-from pyGTC import pyGTC
+import pyGTC
 
-names = "A_\mathrm{X} B_\mathrm{X} C_\mathrm{X} D_\mathrm{X} A_\mathrm{SZ} B_\mathrm{SZ} C_\mathrm{SZ} D_\mathrm{SZ} \rho_\mathrm{SZ-X} \mathsf{\Omega}_\mathrm{m} \ln(10^{10}A_s) h \mathsf{\Omega}_\mathrm{b} n_s \sigma_8".split()
+# List of parameter names, latex-enabled
+names = ['normalization', '$B_\mathrm{X}$', '$C_\mathrm{X}$', '$D_\mathrm{X}$', '$A_\mathrm{SZ}$', '$B_\mathrm{SZ}$', '$C_\mathrm{SZ}$', '$D_\mathrm{SZ}$', '$\\rho_\mathrm{SZ-X}$', '$\mathsf{\Omega}_\mathrm{m}$']
 
-chain = np.loadtxt("samples.txt")
-pyGTC(chains=[chain], param_names=names)
+# List of priors: mean, width
+# List can be shorter than number of parameters
+priors = [[6,38, .61], [.57, .03], [], [.12, .08], [], []]
+
+# List of truth value
+truths = [6, .57, None, .12, None, None, None, None, 0]
+
+# Load chain
+chain1 = np.loadtxt("samples.txt")
+chain2 = np.loadtxt("samples2.txt")
+
+# Labels for the different chains
+chainlabels = ["data1 $\lambda$", "chain 2"]
+
+# Do the magic
+# Unused arguments: confidencelevels, figuresize
+GTC = pyGTC.plotGTC(chains=[chain1, chain2], param_names=names, truths=truths, priors=priors, chainlabels=chainlabels)
+
+#plt.show()
+plt.savefig('GTC.pdf', bbox_inches='tight')
--- pyGTC.py
+++ pyGTC.py
@@ -1,155 +1,268 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import scipy.ndimage
-import ConfigParser
-import glob
-import os
-import sys
 from matplotlib.ticker import MaxNLocator
 from scipy.stats import norm
 
-def pyGTC(chains, param_names=None, priors=None, have_weight=False):
+def plotGTC(chains, param_names=None, truths=None, priors=None, have_weight=False, plotname=None, figuresize=None, chainlabels=None, confidencelevels=2, smoothing_kernel=1):
     
-    if param_names is not None:
-        have_names = True
-    else:
-        have_names = False
+    ##########
+    conflevels = .3173, .0455, .0027
+    if confidencelevels not in [1,2,3]:
+        print "ERROR, confidencelevels must be 1, 2, or 3"
+        return
         
     ##########
-    # Setup figure and color scheme
+    # Setup figure and colors
 
+    plt.rcParams['legend.fontsize'] = 9
     plt.rcParams['axes.labelsize'] = 9
     plt.rcParams['xtick.labelsize'] = 5
     plt.rcParams['ytick.labelsize'] = 5
-    plt.rcParams['legend.fontsize'] = 9
     plt.rcParams['text.usetex']=True
-    plt.rcParams['text.latex.preamble'] = [r'\usepackage{sansmath}',r'\sansmath']
+    plt.rcParams['text.latex.preamble'] = [r'\usepackage{sansmath}', r'\sansmath']
 
-    colors = [['#7fa5e3','#4c72b0'], ['#88db9b','#55a868'], ['#ffc982','#f5964f'], ['#f78185','#c44e52'], ['#b4a5e5','#8172b2'], ['#333333','#000000'], ['#7fa5e3','#4c72b0'], ['#88db9b','#55a868'], ['#ffc982','#f5964f'], ['#f78185','#c44e52'], ['#b4a5e5','#8172b2'], ['#333333','#000000']]
-    maincolors = ['#4c72b0', '#55a868', '#f5964f', '#c44e52', '#8172b2', '#000000', '#4c72b0', '#55a868', '#f5964f', '#c44e52', '#8172b2', '#000000']
+    colors = [['#4c72b0','#7fa5e3','#b2d8ff'],
+        ['#55a868','#88db9b','#bbffce'],
+        ['#f5964f','#ffc982','#fffcb5'],
+        ['#c44e52','#f78185','#ffb4b8'],
+        ['#8172b2','#b4a5e5','#37d8ff'],
+        ['#000000','#333333','#666666']]
+    
     lightblack = '#333333'
+    truthcolor = '#666666'
+    
 
-
-    ##########
-    Nbins = 30.
-    xaxis = np.linspace(0., Nbins**2, Nbins**2)
-
+    ########## Get number of chains and dimensions and take care of sample weights (optional)
+    
+    # Number of chains
     Nchains = len(chains)
+    if Nchains>6:
+        print "ERROR: Currently only supported up to 6 chains"
+        return
 
     # Number of dimensions
     if not have_weight:
-	ndim = len(chains[0][0,:])
-	for i in range(Nchains):
-	    Nsamples = len(chains[i][:,0])
-	    chains[i] = np.insert(chains[i], 0, np.ones(Nsamples), axis=1)
+        ndim = len(chains[0][0,:])
+        for i in range(Nchains):
+            Nsamples = len(chains[i][:,0])
+            chains[i] = np.insert(chains[i], 0, np.ones(Nsamples), axis=1)
     else:
-	ndim = len(chains[0][0,1:])
+        ndim = len(chains[0][0,1:])
+    
+    
+    ##########
+    # These are needed to compute the confidence levels
+    Nbins = 30.
+    xaxis = np.linspace(0., Nbins**2, Nbins**2)
 
-    size_pt = ndim*70.
-    fig_width = size_pt / 72.27
-    figu = plt.figure(figsize=(fig_width,fig_width))
+    # Left and right panel boundaries
+    xmin, xmax = np.empty(ndim), np.empty(ndim)
+    
+    
+    ########## Define the figure size
+    # If no size if given, choose something nice
+    if figuresize is None:
+        fig_width = ndim*70. / 72.27
+    else:
+        if not type(figuresize=='str'):
+            fig_width = figuresize
+        else:
+            if figuresize=='APJ_column':
+                fig_width = 245.26653 / 72.27
+            elif figuresize=='APT_page':
+                fig_width = 513.11743 / 72.27 # TO DO!
+            elif figuresize=='MNRAS_column':
+                fig_width = 240. / 72.27
+            elif figuresize=='MNRAS_page':
+                fig_width = 504. / 72.27
+            else:
+                print "ERROR: Figuresize",figuresize,"not known!"
+                return
 
-    xmin=np.empty(ndim)
-    xmax=np.empty(ndim)
+    fig = plt.figure(figsize=(fig_width,fig_width))
+    
+
+    
 
     ########## 2D contour plots
     for i in range(ndim): # row
         for j in range(ndim): # column
             if j<i:
-                fig = figu.add_subplot(ndim,ndim,(i*ndim)+j+1)
+                ax = fig.add_subplot(ndim,ndim,(i*ndim)+j+1)
             
-                data, extent = [], []
-                sigma1, sigma2 = [], []
+                ##### The actual contour plots
+                data, extent, chainlevels = [], [], []
+
+                # Draw filled contours in reversed order to have first chain in list on top
                 for k in reversed(range(Nchains)):
-                    plotdata, xedges, yedges = np.histogram2d(chains[k][:,j+1], chains[k][:,i+1], weights = chains[k][:,0],bins=Nbins)
-                    extent.append([xedges[0],xedges[-1],yedges[0],yedges[-1]])
+                    # Create 2d histogram
+                    plotdata, xedges, yedges = np.histogram2d(chains[k][:,j+1], chains[k][:,i+1], weights=chains[k][:,0], bins=Nbins)
+                    # image extent
+                    extent.append([xedges[0], xedges[-1], yedges[0], yedges[-1]])
+                    # Normalize
                     plotdata = plotdata/np.sum(plotdata)
+                    # Computed cumulative 1d distribution
                     histogram_ordered = np.sort(plotdata.flat)
                     histogram_cumulative = np.cumsum(histogram_ordered)
-                    temp = np.interp(.32, histogram_cumulative, xaxis)
-                    sigma1temp = np.interp(temp, xaxis, histogram_ordered)
-                    temp = np.interp(.05, histogram_cumulative, xaxis)
-                    sigma2temp = np.interp(temp, xaxis, histogram_ordered)
-                    tempdata = scipy.ndimage.gaussian_filter(plotdata.T, sigma=1)
+                    
+                    # Compute confidence levels (from low to high for technical reasons)
+                    templist = []
+                    for l in reversed(range(confidencelevels)):
+                        # Find location of confidence level in 1d histogram_cumulative
+                        temp = np.interp(conflevels[l], histogram_cumulative, xaxis)
+                        # Find "height" of confidence level
+                        templist.append( np.interp(temp, xaxis, histogram_ordered) )
+                    templist.append(1)
+                    
+                    # Get bin center of histogram edges
                     xbins = np.delete(xedges+.5*(xedges[1]-xedges[0]), -1)
                     ybins = np.delete(yedges+.5*(yedges[1]-yedges[0]), -1)
-                
-                    fig.contourf(xbins,ybins,tempdata, levels=[sigma2temp,sigma1temp,1], colors=colors[k])
-                
+                    
+                    # Apply Gaussian smoothing and plot
+                    tempdata = scipy.ndimage.gaussian_filter(plotdata.T, sigma=smoothing_kernel)
+                    ax.contourf(xbins, ybins, tempdata, levels=templist, colors=colors[k][:confidencelevels][::-1])
+                    
+                    # Store for next step (contour line)
                     data.append(tempdata)
-                    sigma1.append(sigma1temp)
-                    sigma2.append(sigma2temp)
-                for k in range(Nchains):
-                    fig.contour(data[Nchains-1-k], [sigma1[Nchains-1-k]], extent=extent[Nchains-1-k], origin='lower', colors=colors[k][1])
-                    fig.contour(data[Nchains-1-k], [sigma2[Nchains-1-k]], extent=extent[Nchains-1-k], origin='lower', colors=colors[k][0])
+                    chainlevels.append(templist[:-1])
+                    
                 
-            
+                # Draw contour lines in order to see contours lying on top of each other
+                for k in range(Nchains):
+                    for l in range(confidencelevels):
+                        ax.contour(data[Nchains-1-k], [chainlevels[k][l]], extent=extent[Nchains-1-k], origin='lower', colors=colors[k][confidencelevels-1-l])
+                
+                
+                # Truth lines
+                if truths is not None:
+                    if i < len(truths):
+                        if truths[i] is not None:
+                            ax.axhline(truths[i], color=truthcolor, ls='--')
+                    if j < len(truths):
+                        if truths[j] is not None:
+                            ax.axvline(truths[j], color=truthcolor, ls='--')
+                            lo, hi = ax.get_xlim()
+                            if lo>truths[j]: lo = truths[j]-.05*(hi-lo)
+                            if hi<truths[j]: hi = truths[j]+.05*(hi-lo)
+                            ax.set_xlim(lo, hi)
+                            
+                        
                 # Labels and ticklables
                 if i!=ndim-1:
-                    fig.get_xaxis().set_ticklabels([])
+                    ax.get_xaxis().set_ticklabels([])
                 else:
-                    if have_names:
-                        fig.set_xlabel('$'+param_names[j]+'$')
+                    if param_names is not None:
+                        ax.set_xlabel(param_names[j])
                 if j!=0:
-                    fig.get_yaxis().set_ticklabels([])
+                    ax.get_yaxis().set_ticklabels([])
                 else:
-                    if have_names:
-                        fig.set_ylabel('$'+param_names[i]+'$')
+                    if param_names is not None:
+                        ax.set_ylabel(param_names[i])
             
+                # No more than 5 ticks per panel
                 my_locator = MaxNLocator(5)
-                fig.xaxis.set_major_locator(my_locator)
+                ax.xaxis.set_major_locator(my_locator)
                 my_locator = MaxNLocator(5)
-                fig.yaxis.set_major_locator(my_locator)
-            
-                xmin[j], xmax[j] = fig.get_xlim()
+                ax.yaxis.set_major_locator(my_locator)
+                
+                # Limits to be applied to 1d histograms
+                xmin[j], xmax[j] = ax.get_xlim()
             
 
 
     ########## 1D histograms
     for i in range(ndim):
-        fig = figu.add_subplot(ndim,ndim,(i*ndim)+i+1)
+        ax = fig.add_subplot(ndim,ndim,(i*ndim)+i+1)
     
         # 1D histogram
         for k in reversed(range(Nchains)):
+            # create 1d histogram
             hist1d, edges = np.histogram(chains[k][:,i+1], weights = chains[k][:,0], normed=True, bins=Nbins)
+            # Get bin center of histogram edges
             centers = np.delete(edges+.5*(edges[1]-edges[0]), -1)
-            data = scipy.ndimage.gaussian_filter1d((centers,hist1d), sigma=1)
-            plt.fill_between(data[0],data[1],0, color=colors[k][0])
-            plt.plot(data[0],data[1], ls=':', color=colors[k][0])
-    
+            # Gaussian smoothing
+            data = scipy.ndimage.gaussian_filter1d((centers,hist1d), sigma=smoothing_kernel)
+            # Filled histogram
+            plt.fill_between(data[0],data[1],0, color=colors[k][1])
+            # Dotted line for hidden histogram
+            plt.plot(data[0],data[1], ls=':', color=colors[k][1])
+            
+        
+        # Truth line
+        if truths is not None:
+            if i < len(truths):
+                if truths[i] is not None:
+                    ax.axvline(truths[i], color=truthcolor, ls='--')
+                
+                    
+        # Gaussian prior
         if priors is not None:
-            if i < len(width) and width[i]>0:
-                if i==ndim-1:
-                    arr = np.linspace(fig.get_xlim()[0],fig.get_xlim()[1],20)
-                    plt.plot(arr,norm.pdf(arr,mean[i],width[i]), color='k')
-                arr = np.linspace(xmin[i],xmax[i],20)
-                plt.plot(arr,norm.pdf(arr,mean[i],width[i]), color='k')
+            if i < len(priors):
+                if priors[i]:
+                    if priors[i][1]>0:
+                        if i==ndim-1:
+                            arr = np.linspace(ax.get_xlim()[0],ax.get_xlim()[1],40)
+                            plt.plot(arr,norm.pdf(arr,priors[i][0],priors[i][1]), color=lightblack)
+                        else:
+                            arr = np.linspace(xmin[i],xmax[i],40)
+                            plt.plot(arr,norm.pdf(arr,priors[i][0],priors[i][1]), color=lightblack)
+
 
         ########## Ticks, labels, range
-        fig.get_yaxis().set_ticklabels([])
+        # No labels on y-axes, lower limit 0
+        ax.get_yaxis().set_ticklabels([])    
+        ax.set_ylim(bottom=0)
     
-        fig.set_ylim(bottom=0)
-    
+        # Panels in middle of triangle have no x label
         if i!=ndim-1:
-            fig.set_xlim(xmin[i],xmax[i])
-            fig.get_xaxis().set_ticklabels([])
+            ax.set_xlim(xmin[i],xmax[i])
+            ax.get_xaxis().set_ticklabels([])
+        # x label for last panel
         else:
-            if have_names:
-                fig.set_xlabel('$'+param_names[i]+'$')
+            if param_names is not None:
+                ax.set_xlabel(param_names[i])
+        # y label for first panel
         if i==0:
-            if have_names:
-                fig.set_ylabel('$'+param_names[i]+'$')
-                
+            if param_names is not None:
+                ax.set_ylabel(param_names[i])
+        
+        # No more than 5 ticks per panel
         my_locator = MaxNLocator(5)
-        fig.xaxis.set_major_locator(my_locator)
+        ax.xaxis.set_major_locator(my_locator)
         my_locator = MaxNLocator(5)
-        fig.yaxis.set_major_locator(my_locator)
+        ax.yaxis.set_major_locator(my_locator)
     
 
-    #######
 
-    figu.subplots_adjust(hspace=0)
-    figu.subplots_adjust(wspace=0)
+    ########## Legend
+    if chainlabels is not None:
+        # Dummy plot for label line color
+        labelcolors = []
+        ax = fig.add_subplot(ndim,ndim,ndim)
+        
+        # Label for each chain
+        for k in range(Nchains):
+            ax.plot(0,0, color=colors[k][0], label=chainlabels[k])
+            labelcolors.append(colors[k][0])
+        ax.axis('off')
 
-    plt.savefig('GTC.pdf', bbox_inches='tight')
+        # Legend and label colors according to plot
+        leg = plt.legend(loc='upper right', fancybox=True)
+        leg.get_frame().set_alpha(0.)
+        for color,text in zip(labelcolors,leg.get_texts()):
+            text.set_color(color)
+    
+    
+    
+    ##########
 
+    # No space between panels
+    fig.subplots_adjust(hspace=0)
+    fig.subplots_adjust(wspace=0)
+
+    # Save figure
+    if plotname is not None:
+        plt.savefig(plotname, bbox_inches='tight')
+    
+    return fig
