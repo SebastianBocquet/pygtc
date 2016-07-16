2e7e2d06819569a998385d089c97f8deffa1ed3a
Sebastian Bocquet <sbocquet@anl.gov>
2016-07-16 15:04:28 -0500

    cleaned up example script

--- example.py
+++ example.py
@@ -27,37 +27,29 @@ def create_random_samples(ndim, Npoints):
     return samples
 
 
-# List of parameter names, supports latex
-names = ['param name', '$B_\mathrm{\lambda}$', '$C$', '$\\lambda$', 'C', 'D', 'M', '$\\gamma$']
-
-
-# List of priors: mean, width
-# List can be shorter than number of parameters
-priors = [[2, 1], [.5, 2], [], [0, .4], [], []]
-
-
-# List of truth value
-# NOT a python array because of different lengths
-#truths = [4, .5, None, .1, None, None, None, None, 0]
-truths = [[4, .5, None, .1, None, None, None, None, 0], [None, None, .3, 1]]
-
-
 # Create two sets of sample points with 8 parameters and 50000 points
 samples1 = create_random_samples(8, 50000)
 samples2 = 1+create_random_samples(8, 50000)
 
+# List of parameter names, supports latex
+names = ['param name', '$B_\mathrm{\lambda}$', '$C$', '$\\lambda$', 'C', 'D', 'M', '$\\gamma$']
 
 # Labels for the different chains
 ChainLabels = ["data1 $\lambda$", "data 2"]
 
+# List of Gaussian curves to plot (to represent priors): mean, width
+# List can be shorter than number of parameters
+priors = [[2, 1], [.5, 2], [], [0, .4], [], []]
+
+# List of truth values, to mark best-fit or input values
+# NOT a python array because of different lengths
+truths = [[4, .5, None, .1, None, None, None, None, 0], [None, None, .3, 1]]
 
 # Labels for the different truths
 TruthLabels = ['the truth', 'alternative truth']
 
 # Do the magic
-# Unused arguments: NConfidenceLevels, figuresize
 GTC = pyGTC.plotGTC(chains=[samples1,samples2], ParamNames=names, truths=truths, priors=priors, ChainLabels=ChainLabels, TruthLabels=TruthLabels)
-
 
 #plt.show()
 plt.savefig('GTC.pdf', bbox_inches='tight')
