27ea9106b55fa43bafb6ff9b81e9c2eaedcc1e7c
Sebastian Bocquet <sbocquet@anl.gov>
2016-07-17 12:36:48 -0500

    improved handling of sample weights; documentation; style

--- pyGTC.py
+++ pyGTC.py
@@ -10,7 +10,7 @@ def plotGTC(chains, **kwargs):
     Parameters:
     -----------
     chains: 2d array or list of 2d arrays
-        Sample points (Npoints x ndim) drawn from a distribution
+        Sample points (length x Ndim) or multiple sets of samples points
     kwargs:
         See below for all arguments.
 
@@ -19,29 +19,24 @@ def plotGTC(chains, **kwargs):
     fig: matplotlib.figure
         the GTC in all its glory
     """
-    assert len(np.shape(chains)) in [2,3], "Your chains' all fucked up, brah!"
-
-    #increase dimensionality by 1 if user only supplies one chain
-    if len(np.shape(chains)) == 2:
-        chains = [chains]
-
-    #set defaults
-    ParamNames=None
-    truths=None
-    priors=None
-    UseWeights=False
-    PlotName=None
-    FigureSize=None
-    ChainLabels=None
-    TruthLabels=None
-    NConfidenceLevels=2
-    SmoothingKernel=1
-
-    #Read in column names from Pandas DataFrame if exists
+    
+    # Set defaults
+    ParamNames=None # label the x and y axes, supports latex
+    truths=None # Highlight a point (or several) in parameter space by lines
+    priors=None # Draw a Gaussian distribution (or several) in the 1d panels
+    weights=None # Provide weight factors for your sample points
+    PlotName=None # Save plot as PlotName, else return matplotlib.figure object
+    FigureSize=None # Width=height of figure in inches
+    ChainLabels=None # Create legend with names for the plotted data
+    TruthLabels=None # Label the truth lines in the legend
+    NConfidenceLevels=2 # Draw 2d contours out to the NConfidenceLevels level, support 1, 2, 3
+    SmoothingKernel=1 # Gaussian smoothing kernel (in pixels)
+    
+    # Read in column names from Pandas DataFrame if exists
     if hasattr(chains[0], 'columns'):
         ParamNames = chains[0].columns.values
 
-    #parse kwargs
+    # Parse kwargs
     if kwargs is not None:
         for key, val in kwargs.iteritems():
             if key == 'ParamNames':
@@ -50,8 +45,8 @@ def plotGTC(chains, **kwargs):
                 truths = val
             if key == 'priors':
                 priors = val
-            if key == 'UseWeights':
-                UseWeights = val
+            if key == 'weights':
+                weights = val
             if key == 'PlotName':
                 PlotName = val
             if key == 'FigureSize':
@@ -66,61 +61,60 @@ def plotGTC(chains, **kwargs):
             if key == 'SmoothingKernel':
                 SmoothingKernel = val
         
-    
-
-    ##########
-    #Magic numbers defining Gaussian confidence levels
-    GaussConfLevels = [.3173, .0455, .0027]
-
-
-    ##########
     # Setup figure and colors
-
     plt.rcParams['legend.fontsize'] = 9
     plt.rcParams['axes.labelsize'] = 9
     plt.rcParams['xtick.labelsize'] = 5
     plt.rcParams['ytick.labelsize'] = 5
     plt.rcParams['text.usetex'] = True
     plt.rcParams['text.latex.preamble'] = [r'\usepackage{sansmath}', r'\sansmath']
-
     colors = [['#4c72b0','#7fa5e3','#b2d8ff'],
         ['#55a868','#88db9b','#bbffce'],
         ['#f5964f','#ffc982','#fffcb5'],
         ['#c44e52','#f78185','#ffb4b8'],
         ['#8172b2','#b4a5e5','#37d8ff'],
         ['#000000','#333333','#666666']]
-
     LightBlack = '#333333'
-
     TruthColor = ['r','c','g','b','m']
+    
+    # Increase dimensionality of chains by 1 if user only supplies one chain
+    if len(np.shape(chains)) == 2:
+        chains = [chains] 
 
+    # Number of chains
+    Nchains = len(chains)
+    assert Nchains<len(colors), "ERROR: Currently only supported up to "+str(len(colors))+" chains"
+    
+    # Check that chains are 2d arrays
+    for i in range(Nchains):
+        assert len(np.shape(chains[i]))==2, "Your chain "+str(i)+" all fucked up, brah!"
+    
+    # Number of dimensions
+    ndim = len(chains[0][0,:])
+    
+    # Manage the sample point weights
+    if weights==None:
+        # Set unit weights if no weights are provided
+        weights = []
+        for i in range(Nchains):
+            weights.append( np.ones(len(chains[i])) )
+    else:
+        if len(np.shape(weights))==len(chains[0]):
+            weights = [weights]
+        for i in range(Nchains):
+            if len(weights[i])!=len(chains[i]):
+                raise ValueError("missmatch in chain/weights #%d: len(chain) %d, len(weights) %d"%(i,len(chains[i]),len(weights[i])))
 
-    ##########
+    #Magic numbers defining Gaussian confidence levels
+    GaussConfLevels = [.3173, .0455, .0027]
+
     # Increase dimensionality of truth list by one if single list
     if truths is not None:
         if not isinstance(truths[0], list):
             truths = [truths]
         else:
             assert len(truths)<len(TruthColor), "ERROR: Currently only supported up to "+str(len(TruthColor))+" truths"
-            
-    
-    ########## Get number of chains and dimensions and take care of sample weights (optional)
 
-    # Number of chains
-    Nchains = len(chains)
-    assert Nchains<len(colors), "ERROR: Currently only supported up to "+str(len(colors))+" chains"
-
-    # Number of dimensions
-    if not UseWeights:
-        ndim = len(chains[0][0,:])
-        for i in range(Nchains):
-            Nsamples = len(chains[i][:,0])
-            chains[i] = np.insert(chains[i], 0, np.ones(Nsamples), axis=1)
-    else:
-        ndim = len(chains[0][0,1:])
-
-
-    ##########
     # These are needed to compute the confidence levels
     Nbins = 30.
     NbinsFlat = np.linspace(0., Nbins**2, Nbins**2)
@@ -128,33 +122,32 @@ def plotGTC(chains, **kwargs):
     # Left and right panel boundaries
     xmin, xmax = np.empty(ndim), np.empty(ndim)
 
-
-    ########## Define the figure size
-    # If no size if given, choose something nice
+    # Figure size (or resolution)
     if FigureSize is None:
+        # If no figure size is given, use resolution of 70 ppp (pixel per panel)
         fig_width = ndim*70. / 72.27
     else:
+        # User-defined width=height in inches
         if not type(FigureSize=='str'):
             fig_width = FigureSize
         else:
+            # Choose from a couple of presets to fit your publication
             if FigureSize=='APJ_column':
                 fig_width = 245.26653 / 72.27
-            elif FigureSize=='APT_page':
-                fig_width = 513.11743 / 72.27 # TO DO!
+            elif FigureSize=='APJ_page':
+                fig_width = 513.11743 / 72.27
             elif FigureSize=='MNRAS_column':
                 fig_width = 240. / 72.27
             elif FigureSize=='MNRAS_page':
                 fig_width = 504. / 72.27
             else:
-                print "ERROR: Figuresize",FigureSize,"not known!"
-                return
-
+                raise ValueError("Figuresize %s unknown!"%FigureSize)
     fig = plt.figure(figsize=(fig_width,fig_width))
 
 
 
-
     ########## 2D contour plots
+
     ChainLevels = np.ones((Nchains,NConfidenceLevels+1))
     extents = np.empty((Nchains,4))
     
@@ -163,13 +156,13 @@ def plotGTC(chains, **kwargs):
             if j<i:
                 ax = fig.add_subplot(ndim,ndim,(i*ndim)+j+1)
 
-                ##### The actual contour plots
+
+                ##### The filled contour plots
                 SmoothData = []
-                
                 # Draw filled contours in reversed order to have first chain in list on top
                 for k in reversed(range(Nchains)):
                     # Create 2d histogram
-                    hist2d, xedges, yedges = np.histogram2d(chains[k][:,j+1], chains[k][:,i+1], weights=chains[k][:,0], bins=Nbins)
+                    hist2d, xedges, yedges = np.histogram2d(chains[k][:,j], chains[k][:,i], weights=weights[k], bins=Nbins)
                     # image extent, needed below for contour lines
                     extents[k] = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
                     # Normalize
@@ -194,39 +187,46 @@ def plotGTC(chains, **kwargs):
                     ax.contourf(xbins, ybins, SmoothData[-1], levels=ChainLevels[k], colors=colors[k][:NConfidenceLevels][::-1])
 
 
-                # Draw contour lines in order to see contours lying on top of each other
+                ###### Draw contour lines in order to see contours lying on top of each other
                 for k in range(Nchains):
                     for l in range(NConfidenceLevels):
                         ax.contour(SmoothData[Nchains-1-k], [ChainLevels[k][NConfidenceLevels-1-l]], extent=extents[k], origin='lower', colors=colors[k][l])
 
 
-                # Truth lines
+                ##### Truth lines
                 if truths is not None:
                     for k in range(len(truths)):
+                        # horizontal line
                         if i < len(truths[k]):
                             if truths[k][i] is not None:
                                 ax.axhline(truths[k][i], color=TruthColor[k])
+                        # vertical line
                         if j < len(truths[k]):
                             if truths[k][j] is not None:
                                 ax.axvline(truths[k][j], color=TruthColor[k])
+                                # If needed, readjust limits of x_axis
                                 lo, hi = ax.get_xlim()
                                 if lo>truths[k][j]: lo = truths[k][j]-.05*(hi-lo)
                                 if hi<truths[k][j]: hi = truths[k][j]+.05*(hi-lo)
                                 ax.set_xlim(lo, hi)
 
 
-                # Labels and ticklables
-                if i!=ndim-1:
-                    ax.get_xaxis().set_ticklabels([])
-                else:
+                ##### Ticks & labels
+                
+                # x-labels at bottom of plot only
+                if i==ndim-1:
                     if ParamNames is not None:
                         ax.set_xlabel(ParamNames[j])
-                if j!=0:
-                    ax.get_yaxis().set_ticklabels([])
                 else:
+                    ax.get_xaxis().set_ticklabels([])
+                    
+                # y-labels for left-most panels only
+                if j==0:
                     if ParamNames is not None:
                         ax.set_ylabel(ParamNames[i])
-
+                else:
+                    ax.get_yaxis().set_ticklabels([])
+                
                 # No more than 5 ticks per panel
                 my_locator = MaxNLocator(5)
                 ax.xaxis.set_major_locator(my_locator)
@@ -242,10 +242,10 @@ def plotGTC(chains, **kwargs):
     for i in range(ndim):
         ax = fig.add_subplot(ndim,ndim,(i*ndim)+i+1)
 
-        # 1D histogram
+        ##### 1D histogram
         for k in reversed(range(Nchains)):
             # create 1d histogram
-            hist1d, edges = np.histogram(chains[k][:,i+1], weights = chains[k][:,0], normed=True, bins=Nbins)
+            hist1d, edges = np.histogram(chains[k][:,i], weights = weights[k], normed=True, bins=Nbins)
             # Bin center between histogram edges
             centers = np.delete(edges+.5*(edges[1]-edges[0]), -1)
             # Gaussian smoothing
@@ -256,7 +256,7 @@ def plotGTC(chains, **kwargs):
             plt.plot(PlotData[0], PlotData[1], ls=':', color=colors[k][1])
 
 
-        # Truth line
+        ##### Truth line
         if truths is not None:
             for k in range(len(truths)):
                 if i < len(truths[k]):
@@ -264,7 +264,7 @@ def plotGTC(chains, **kwargs):
                         ax.axvline(truths[k][i], color=TruthColor[k])
 
 
-        # Gaussian prior
+        ##### Gaussian prior
         if priors is not None:
             if i < len(priors):
                 if priors[i]:
@@ -277,20 +277,21 @@ def plotGTC(chains, **kwargs):
                             plt.plot(arr,norm.pdf(arr,priors[i][0],priors[i][1]), color=LightBlack)
 
 
-        ########## Ticks, labels, range
+        ##### Ticks, labels, range
+        
         # No labels on y-axes, lower limit 0
         ax.get_yaxis().set_ticklabels([])
         ax.set_ylim(bottom=0)
-
-        # Panels in middle of triangle have no x label
-        if i!=ndim-1:
-            ax.set_xlim(xmin[i],xmax[i])
-            ax.get_xaxis().set_ticklabels([])
-        # x label for last panel
-        else:
+        
+        # x-label for bottom-right panel only
+        if i==ndim-1:
             if ParamNames is not None:
                 ax.set_xlabel(ParamNames[i])
-        # y label for first panel
+        else:
+            ax.set_xlim(xmin[i],xmax[i])
+            ax.get_xaxis().set_ticklabels([])        
+        
+        # y label for top-left panel
         if i==0:
             if ParamNames is not None:
                 ax.set_ylabel(ParamNames[i])
@@ -305,26 +306,26 @@ def plotGTC(chains, **kwargs):
 
     ########## Legend
     if (ChainLabels is not None) or (TruthLabels is not None):
-        # Dummy plot for label line color
+        ##### Dummy plot for label line color
         LabelColors = []
         ax = fig.add_subplot(ndim,ndim,ndim)
         ax.axis('off')
     
-        # Label the data sets
+        ##### Label the data sets
         if ChainLabels is not None:
             # Label for each chain
             for k in range(Nchains):
                 ax.plot(0,0, color=colors[k][0], label=ChainLabels[k])
                 LabelColors.append(colors[k][0])
         
-        # Label the truth lines
+        ##### Label the truth lines
         if TruthLabels is not None:
             # Label for each truth
             for k in range(len(TruthLabels)):
                 ax.plot(0,0, color=TruthColor[k], label=TruthLabels[k])
                 LabelColors.append(TruthColor[k])
 
-        # Legend and label colors according to plot
+        ##### Legend and label colors according to plot
         leg = plt.legend(loc='upper right', fancybox=True)
         leg.get_frame().set_alpha(0.)
         for color,text in zip(LabelColors,leg.get_texts()):
