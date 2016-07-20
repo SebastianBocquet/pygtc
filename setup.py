1a6920a9903745ba9a2ff49f84494ed76a6d9db8
Faustin Carter <faustin.carter@gmail.com>
2016-07-20 17:45:46 -0500

    adding files for packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import pygtc
+
+setup(
+    name = 'pyGTC',
+    description = 'Make an awesome giant triangle confusogram (gtc)!',
+    version = pygtc.__version__,
+    packages = [pygtc],
+    long_description = open('README.rst').read(),
+    install_requires = ['numpy', 'matplotlib', 'scipy'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Visualization'
+    ]
+
+)
