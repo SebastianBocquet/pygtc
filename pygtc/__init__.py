a4393995be834a63758068bde030b102e3dd46fd
Sebastian Bocquet <sbocquet@anl.gov>
2016-07-21 09:38:36 -0500

    fixed import for __all__

--- pygtc/__init__.py
+++ pygtc/__init__.py
@@ -1,13 +1,13 @@
 __version__ = "0.1.0"
 __author__ = "Sebastian Bocquet and Faustin W. Carter"
 __contributors__ = [
-    'Person 1',
-    'Person 2',
+    'Samuel Flender',
+    'Ben Hoyle',
     'etc'
 ]
 
 
 
-__all__ = [plotGTC]
+__all__ = ['plotGTC']
 
 from .pygtc import plotGTC
