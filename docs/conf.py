7b06fc0f6d3bbbfe6fe16d1f5a3d59075aa2b755
Faustin Carter <faustin.carter@gmail.com>
2016-07-22 00:15:27 -0500

    more docs nonsense

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,8 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
+import pygtc
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
