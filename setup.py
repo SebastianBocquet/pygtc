3639a1d08a648e265cdb95aab6ddb0fe10d0d6a1
Faustin Carter <faustin.carter@gmail.com>
2020-04-16 22:30:03 -0700

    linting fixes for setup and init

--- setup.py
+++ setup.py
@@ -5,23 +5,23 @@ with open(os.path.join('pygtc', 'VERSION.txt')) as version_file:
     version_number = version_file.read().strip()
 
 setup(
-    name = 'pyGTC',
-    description = 'Make an awesome giant triangle confusogram (gtc)!',
-    version = version_number,
-    author = 'Sebastian Bocquet and Faustin Carter',
-    author_email = 'sebastian.bocquet@gmail.com',
-    license = 'MIT',
-    url = 'http://github.com/sebastianbocquet/pygtc',
-    packages = ['pygtc'],
-    package_data = {'pygtc': ['VERSION.txt',
-                              'tests/*.py',
-                              'tests/baseline_images/test_plotGTC/*.png']},
-    long_description = open('README.rst').read(),
-    install_requires = [
+    name='pyGTC',
+    description='Make an awesome giant triangle confusogram (gtc)!',
+    version=version_number,
+    author='Sebastian Bocquet and Faustin Carter',
+    author_email='sebastian.bocquet@gmail.com',
+    license='MIT',
+    url='http://github.com/sebastianbocquet/pygtc',
+    packages=['pygtc'],
+    package_data={'pygtc': ['VERSION.txt',
+                            'tests/*.py',
+                            'tests/baseline_images/test_plotGTC/*.png']},
+    long_description=open('README.rst').read(),
+    install_requires=[
         'numpy',
         'matplotlib'
     ],
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
